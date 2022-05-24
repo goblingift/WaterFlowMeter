3c8240d732aeb0c6e37c7d45634554c52b81f2c9
André Keßler <andre.kessler91@gmx.de>
2022-05-24 12:17:34 +0200

    Converted to VS-Code compatible code

--- src/SimpleWaterFlow.cpp
+++ src/SimpleWaterFlow.cpp
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include <U8g2lib.h>
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
@@ -17,7 +18,7 @@ void IRAM_ATTR pulseCounter()
 
 void setup() {
    pinMode(signalPin, INPUT);
-   Serial.begin(115200); 
+   Serial.begin(9600); 
 
    u8g2.begin();
    
